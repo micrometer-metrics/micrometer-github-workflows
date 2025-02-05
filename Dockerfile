f71d6d64739edebb621cb2906bdcf0a78ae12810
Marcin Grzejszczak <marcin@grzejszczak.pl>
2025-02-05 20:36:09 +0100

    Simplified the build, follow the approach Spring Changelog Generator went

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM gradle:8.5.0-jdk17-alpine as build
+COPY gradle /app/gradle/
+COPY src /app/src/
+COPY config /app/config/
+COPY build.gradle settings.gradle gradle.properties /app/
+RUN cd /app && gradle -Dorg.gradle.welcome=never --no-daemon shadowJar
+
+FROM ghcr.io/bell-sw/liberica-openjre-debian:17.0.10-13
+COPY --from=build /app/build/libs/micrometer-release.jar /opt/action/micrometer-release.jar
+ENTRYPOINT ["java", "-jar", "/opt/action/micrometer-release.jar"]
---
+++ src/main/java/io/micrometer/release/Main.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2025 Broadcom.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micrometer.release;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // Env Vars
+        // GH_TOKEN
+        // CHANGELOG_GENERATOR_VERSION
+        // GITHUB_REPOSITORY
+        // GITHUB_REF_NAME
+        // PREVIOUS_REF_NAME
+        // BSKY_IDENTIFIER
+        // BSKY_HANDLE
+        // SPRING_RELEASE_GCHAT_WEBHOOK_URL
+        newWorkflow().run();
+    }
+
+    private static PostReleaseWorkflow newWorkflow() {
+        ProcessRunner processRunner = new ProcessRunner();
+        return new PostReleaseWorkflow(new ChangelogGeneratorDownloader(), new ChangelogGenerator(processRunner),
+                new ChangelogFetcher(processRunner), new ChangelogProcessor(processRunner),
+                new ReleaseNotesUpdater(processRunner), new MilestoneUpdater(processRunner), new NotificationSender());
+    }
+
+}
