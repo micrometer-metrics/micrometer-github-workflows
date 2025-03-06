d19d88f589a309ea912ee6876213d126a7b09cde
Marcin Grzejszczak <marcin@grzejszczak.pl>
2025-03-06 16:54:30 +0100

    Fixed formatting

--- src/e2eTest/java/io/micrometer/release/single/SingleProjectGithubActionsE2eTests.java
+++ src/e2eTest/java/io/micrometer/release/single/SingleProjectGithubActionsE2eTests.java
@@ -28,8 +28,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class SingleProjectGithubActionsE2eTests implements GithubActions {
 
-    String[] closedIssueTitles = {"Closed issue in generic 0.1.x", "Closed bug in concrete 0.1.1",
-        "Closed bug in generic 0.1.x", "Closed enhancement in generic 0.1.x"};
+    String[] closedIssueTitles = { "Closed issue in generic 0.1.x", "Closed bug in concrete 0.1.1",
+            "Closed bug in generic 0.1.x", "Closed enhancement in generic 0.1.x" };
 
     @BeforeAll
     static void should_go_through_whole_flow() {
@@ -44,9 +44,9 @@ class SingleProjectGithubActionsE2eTests implements GithubActions {
         assertThat(release.body()).containsIgnoringWhitespaces(
                 "Closed enhancement in generic 0.1.x [#8](https://github.com/marcingrzejszczak/gh-actions-test/issues/8)")
             .containsIgnoringWhitespaces(
-                "Closed bug in concrete 0.1.1 [#12](https://github.com/marcingrzejszczak/gh-actions-test/issues/12)")
+                    "Closed bug in concrete 0.1.1 [#12](https://github.com/marcingrzejszczak/gh-actions-test/issues/12)")
             .containsIgnoringWhitespaces(
-                "Closed bug in generic 0.1.x [#9](https://github.com/marcingrzejszczak/gh-actions-test/issues/9)");
+                    "Closed bug in generic 0.1.x [#9](https://github.com/marcingrzejszczak/gh-actions-test/issues/9)");
     }
 
     @Test
@@ -68,11 +68,9 @@ class SingleProjectGithubActionsE2eTests implements GithubActions {
         Milestone milestone = githubClient.getMilestoneByTitle("0.1.2");
 
         assertThat(milestone.state()).isEqualTo("open");
-        assertThat(milestone.dueOn()).isEqualTo(
-            ReleaseDateCalculator.calculateDueDate(LocalDate.now()));
+        assertThat(milestone.dueOn()).isEqualTo(ReleaseDateCalculator.calculateDueDate(LocalDate.now()));
         List<Issue> issues = githubClient.getIssuesForMilestone(milestone.number());
-        assertThat(issues).filteredOn(
-                s -> "Open issue in concrete 0.1.1".equalsIgnoreCase(s.title()))
+        assertThat(issues).filteredOn(s -> "Open issue in concrete 0.1.1".equalsIgnoreCase(s.title()))
             .extracting(Issue::state)
             .containsOnly("open");
         assertThat(issues).extracting(Issue::title)
@@ -85,17 +83,14 @@ class SingleProjectGithubActionsE2eTests implements GithubActions {
         Milestone milestone = githubClient.getMilestoneByTitle("0.1.x");
 
         List<Issue> closedIssues = githubClient.getClosedIssuesForMilestone(milestone.number());
-        assertThat(closedIssues)
-            .extracting(Issue::title)
-            .matches(strings -> strings.stream()
-                    .noneMatch(s -> s.contains("in generic") || s.contains("in concrete")),
-                "Must not contain any closed issues that we created");
+        assertThat(closedIssues).extracting(Issue::title)
+            .matches(strings -> strings.stream().noneMatch(s -> s.contains("in generic") || s.contains("in concrete")),
+                    "Must not contain any closed issues that we created");
     }
 
     private static void runPostReleaseWorkflow() {
         log.info("Running post release action from tag");
-        GithubActions.runWorkflow("post-release-workflow.yml", "v0.1.1",
-            List.of("gh", "workflow", "run",
+        GithubActions.runWorkflow("post-release-workflow.yml", "v0.1.1", List.of("gh", "workflow", "run",
                 "post-release-workflow.yml", "--ref", "v0.1.1", "-f", "previous_ref_name=v0.1.0"));
     }
 
