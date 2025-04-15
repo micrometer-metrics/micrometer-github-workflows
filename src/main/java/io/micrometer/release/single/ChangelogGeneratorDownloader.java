7c1bdaef0a4e7df5df9a15a6b7178cddc4a20ec0
Tommy Ludwig <8924140+shakuzen@users.noreply.github.com>
2025-04-15 17:04:50 +0900

    Upgrade changelog-generator to 0.0.12
    Fixes an issue we're having with an error response from the GitHub API when generating the release notes for a milestone with enough issues to have more than one page.
    
    Signed-off-by: Tommy Ludwig <8924140+shakuzen@users.noreply.github.com>

--- src/main/java/io/micrometer/release/single/ChangelogGeneratorDownloader.java
+++ src/main/java/io/micrometer/release/single/ChangelogGeneratorDownloader.java
@@ -34,7 +34,7 @@ class ChangelogGeneratorDownloader {
 
     private static final String CHANGELOG_GENERATOR_JAR = "github-changelog-generator.jar";
 
-    private static final String DEFAULT_CHANGELOG_GENERATOR_VERSION = "0.0.11";
+    private static final String DEFAULT_CHANGELOG_GENERATOR_VERSION = "0.0.12";
 
     static final String CHANGELOG_GENERATOR_URL = "https://github.com/spring-io/github-changelog-generator/releases/download/v%s/github-changelog-generator.jar";
 
